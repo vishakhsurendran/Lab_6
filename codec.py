db019eb878a056193b946f551f85a70fc6bfadaf
Vishakh Surendran <vishakhsurendran@ufl.edu>
2023-10-24 13:35:49 -0400

    added comments to program

--- codec.py
+++ codec.py
@@ -1,33 +1,33 @@
   # Vishakh Surendran
 def encoder(password):  # method to encode a password by adding three to each digit
-    encoded_password = ""
-    for i in password:
+    encoded_password = ""  # creates empty string to hold encoded password
+    for i in password:  # iterates through password and adds 3 to each digit
         encoded_digit = int(i) + 3
-        encoded_password += str(encoded_digit)
-    return encoded_password
+        encoded_password += str(encoded_digit)  # adds encoded digit to variable
+    return encoded_password  # returns encoded password
 
 
 def decoder(code):
     pass
 
 
-def main():
-    continue_codec = True
-    while continue_codec:
-        print("Menu")
+def main():  # main function
+    continue_codec = True  # continues program
+    while continue_codec:  # while loop to loop program
+        print("Menu")  # prints menu with options for user
         print("-------------")
         print("1. Encode")
         print("2. Decode")
         print("3. Quit\n")
-        choice = int(input("Please enter an option: "))
-        if choice == 1:
+        choice = int(input("Please enter an option: "))  # accepts int input for user choice
+        if choice == 1:  # if choice is 1, encodes user provided password
             password = str(input("Please enter your password to encode: "))
-            encoded = encoder(password)
+            encoded = encoder(password)  # stores encoded password to variable
             print("Your password has been encoded and stored!\n")
-        elif choice == 2:
-            decoded = decoder(encoded)
-            print("The encoded password is " + encoded + " and the original password is " + decoded + ".\n")
-        elif choice == 3:
+        elif choice == 2:  # if choice is 2, decodes encoded password
+            decoded = decoder(encoded)  # decodes encoded password and stores value
+            print("The encoded password is " + encoded + " and the original password is " + decoded + ".\n")  # prints statement with both encoded and original passwords
+        elif choice == 3:  # if choice is 3, ends program
             continue_codec = False
 
 
